79678fab56e01937e294367d4591cdd1585a67fa
Lovasi János <97015824+lovasij@users.noreply.github.com>
2023-11-18 10:22:36 +0100

    forrásfájlok, feladat feltöltése

---
+++ fuggvenyek.py
@@ -0,0 +1,38 @@
+""""
+A fuggvenyek modul tartalmazza azokat a speciális függvényeket a tételeken túl, amelyek szükségesek a feladat megoldásához:
+- fájl beolvasása
+- összes érem meghatározása
+- összes pont meghatározása
+- kiírás fájba
+"""
+
+# fájl beolvasása
+def beolvas():
+    """
+    csv fájl beolvasása listába, a függvény kimenete a lista
+    """
+
+# Az egy olimpián gyűjtött éremek számának meghatározása
+def ossz_erem():
+    """
+    Az arany, ezüst és bronz érmek összege.
+    A függvény felhasználja a tetelek modul összegzés tételét.
+    """
+
+# Az egy olimpián szerzett pontszám meghatározása
+def ossz_pont():
+    """
+    Összes pontszám egyenlő:
+    arany éremek száma szorozva 7-tel,
+    ezüst éremek száma szorozva 5-tel,
+    a bronz éremek száma szorozva 4-gyel,
+    a negyedik helyek száma szorozva 3-ma,
+    az ötödik helyek száma szorozva 2-vel,
+    valamint a hatodik helyek száma szorozva 1-gyel.
+    """
+
+# adatok kiíratása fájlba
+def kiir():
+    """
+    adatok kiíratása csv fájlba
+    """
---
+++ tetelek.py
@@ -0,0 +1,68 @@
+"""
+A modul az alábbi programozási tételek implementációját tartalmazza:
+- megszámlálás tétele
+- összegzés tétele
+- maximumkiválasztás tétele
+- minimumkiválasztás tétele
+- eldöntés tétele
+- kiválogatás tétele
+- lineáris keresés tétele
+"""
+# megszámlálás tétele - függvény
+def megszamlalas(M, T):
+    """
+    Adott egy n elemű M sorozat és egy T tulajdonság.
+    Meghatározzuk a T tulajdonsággal rendelkező elemek számát!
+    A függvény visszatérési értéke adja meg ezt!
+    """
+
+# összegzés tétele - függvény
+def osszegzes(T):
+    """
+    Adott egy n elemű T sorozat.
+    Meghatározzuk a T sorozat elemeinek összegét!
+    A függvény visszatérési értéke adja meg ezt!
+    """
+
+# maximumkiválasztás tétele - függvény
+def maxkivalaszt(T):
+    """
+    Adott egy n elemű T sorozat.
+    Megkeressük meg a sorozat legnagyobb elemét!
+    A függvény visszatérési értéke adja meg ezt!
+    """
+
+# minimum kiválasztás tétele - függvény
+def minkivalaszt(T):
+    """
+    Adott egy n elemű T sorozat.
+    Megkeressük meg a sorozat legkisebb elemét!
+    A függvény visszatérési értéke adja meg ezt!
+    """
+
+# eldöntés tétele - függvény
+def eldontes(M, T):
+    """
+    Adott egy n elemű M sorozat, és az elemein értelmezett T tulajdonság.
+    Eldöntjük el, hogy van-e legalább egy olyan elem a sorozatban,
+    amely rendelkezik a T tulajdonsággal!
+    A függvény visszatérési értéke (True/Falsee) adja meg ezt!
+    """
+
+# kiválogatás tétele - függvény
+def kivalogat(M, T):
+    """
+    Adott egy n elemű M sorozat és egy T tulajdonság.
+    Gyűjtsük ki az A sorozatba a T tulajdonságú M-beli elemeket!
+    A függvény visszatérési érétke (A) adja meg ezt!
+    """
+
+# lineáris keresés tétele - függvény
+def keres(M, T):
+    """
+    Adott egy n elemű M sorozat és egy T tulajdonság.
+    Megkeressük az első olyan elemét M-nek, amely rendelkezik a T tulajdonsággal,
+    és megadjuk, hogy hányadik!
+    A függvény visszatérési értéke adja meg ezt!
+    """
+    
