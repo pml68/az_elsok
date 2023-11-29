cd1b9ef06254a862c4aa4f17eb026c9d6094f859
Prisznyák Patrik <priszny.patrik.204@gr-szechenyi.inside>
2023-11-29 11:50:56 +0100

    add: ossz_erem, ossz_pont variable

--- olimpia.py
+++ olimpia.py
@@ -3,7 +3,16 @@ import fuggvenyek
 
 def main():
     olimpiak = fuggvenyek.beolvas("olimpia.csv")
-    print(olimpiak)
+    arany_oszlop = olimpiak["arany"]
+    ossz_erem_minden_olimpia = []
+    ossz_pont_minden_olimpia = []
+    for i in range(len(arany_oszlop)):
+        ermek = fuggvenyek.ossz_erem(olimpiak, i+1)
+        ffs = fuggvenyek.fourthFifthSixth(olimpiak, i+1)
+        pontok = fuggvenyek.ossz_pont(ermek, ffs)
+        ossz_erem_minden_olimpia.append(ermek)
+        ossz_pont_minden_olimpia.append(pontok)
 
 if __name__ == "__main__":
     main()
+  
